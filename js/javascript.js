dc82be926e660bb7d41c568435f8f252f484fd43
Tylenis <nerijuskret@gmail.com>
2018-06-23 22:18:19 +0300

    Add infowindows

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,4 @@
-var location_data = [
+const location_data = [
     {title: 'Švyturio Arena', location: {lat: 55.68745448842839, lng: 21.15200260962421}},
     {title: 'Clock and Watch Museum', location: {lat: 55.712415452967925, lng: 21.134070239354646}},
     {title: 'Smiltynė Beach', location: {lat: 55.70393548282197, lng: 21.099023948507458}},
@@ -8,13 +8,19 @@ var location_data = [
     {title: '"Meridianas Tall Ship" restaurant', location: {lat: 55.71020684190002, lng: 21.134616721326314}}
 ];
 
-var map_center = {lat: 55.703297, lng: 21.144279};
+const CLIENT_ID = '4ECKO5WEEW3HD03K0YOHG2HSPPRGJC1MZFFKH0N2TD5YY513';
+const CLIENT_SECRET = 'QNBN5OSNQY4HAFX2IVDVESWFGRBLXX1KXQSXZANWXDB4I0AO';
+const FOURSQUARE_API_URL = 'https://api.foursquare.com/v2/venues/';
+
+const map_center = {lat: 55.703297, lng: 21.144279};
 
 var AppViewModel = function(map){
-    var self = this;
+    let self = this;
     self.map = map;
     self.markers = [];
     self.locations = ko.observableArray([]);
+
+    let infowindow = new google.maps.InfoWindow();
 
     self.init = function(){
         // Initialize app.
@@ -36,7 +42,10 @@ var AppViewModel = function(map){
         // Create list of markers.
         self.locations().forEach(function(location){
             if(location.show()){
-                var marker = new google.maps.Marker({position: location.location});
+                var marker = new google.maps.Marker({
+                    position: location.location,
+                    title: location.title
+                });
                 self.markers.push(marker);
             }
         });
@@ -44,13 +53,16 @@ var AppViewModel = function(map){
 
     self.showMarkers = function(){
         // Add markers to map and center map accordingly.
-        var bounds = new google.maps.LatLngBounds();
+        let bounds = new google.maps.LatLngBounds();
         for(var i = 0; i<self.markers.length; i++){
             self.markers[i].setMap(self.map);
             bounds.extend(self.markers[i].position);
+            self.markers[i].addListener('click', function(){
+                self.openInfoWindow(this, infowindow);
+            });
         }
         if(self.markers.length === 1){
-            var marker_position = self.markers[0].position;
+            let marker_position = self.markers[0].position;
             self.map.setCenter(marker_position);
             self.map.setZoom(16);
         } else if(self.markers.length === 0){
@@ -69,9 +81,87 @@ var AppViewModel = function(map){
         self.markers = [];
     };
 
+    self.loadImage = function(venueid){
+        $.ajax({
+            url: FOURSQUARE_API_URL+venueid+'/photos',
+            data: {
+                'client_id': CLIENT_ID,
+                'client_secret': CLIENT_SECRET,
+                'limit': 1,
+                'v': '20180623'
+            },
+            success: function(data){
+                let photoObj = data.response.photos.items[0]
+                let photoUrl = photoObj.prefix +'320x192'+ photoObj.suffix
+                let imgElement = `<img src="${photoUrl}">`;
+                $('.spinner-container').remove();
+                $('.img-container').html(imgElement);
+            },
+            error: function(error){
+                console.log(error);
+            }
+        });
+    };
+
+    self.loadInfoWindowContent = function(){
+        // Get content from FourSquare API and populate infowindow.
+        let lat = infowindow.marker.position.lat();
+        let lng = infowindow.marker.position.lng();
+
+        $.ajax({
+            url: FOURSQUARE_API_URL+'search',
+            data: {
+                'll': lat+','+lng,
+                'client_id': CLIENT_ID,
+                'client_secret': CLIENT_SECRET,
+                'limit': 1,
+                'v': '20180623'
+            },
+            headers: {
+                'Accept-Language': 'en',
+            },
+            success: function(data){
+                let venue = data.response.venues[0];
+                let venueAddress = venue.location.formattedAddress.join(', ');
+                let addressElement = `<p>${venueAddress}</p>`;
+                let venueId = venue.id;
+                self.loadImage(venueId);
+                $('.address').html(addressElement);
+            },
+            error: function(data){
+                console.log(data);
+            }
+        });
+    };
+
+    self.openInfoWindow = function(marker, infowindow){
+        let spinner = `
+            <div class="spinner-container">    
+                <i class="fas fa-spinner fa-spin fa-3x"></i>
+            </div>`;
+        let template = `
+            <div class="infowindow">
+                <div class="img-container">
+                    ${spinner}
+                </div>
+                <h5 class="info-title">${marker.title}</h5>
+                <div class="address">
+                </div>
+            </div>`;
+        if(infowindow.marker != marker){
+            infowindow.marker = marker;
+            infowindow.setContent(template);
+            infowindow.open(self.map, marker);
+            self.loadInfoWindowContent();
+            infowindow.addListener('closeclick', function(){
+                infowindow.marker = null;
+            });
+        }
+    };
+
     self.filter = function(){
         // Filter out list and markers.
-        var expression = new RegExp(this.value, 'i');
+        let expression = new RegExp(this.value, 'i');
         self.locations().forEach(function(item){
             if(expression.test(item.title)){
                 item.show(true);
@@ -89,7 +179,7 @@ var AppViewModel = function(map){
 
 function googleSuccess(){
     // Create a new map.
-    var map = new google.maps.Map(document.getElementById('map'),{
+    let map = new google.maps.Map(document.getElementById('map'),{
         center: map_center,
         zoom: 14,
         mapTypeControl: true,
@@ -103,7 +193,7 @@ function googleSuccess(){
 };
 
 function googleError(){
-    var text = '<h3 id="error-msg">Sorry, we couldn\'t load the map.</h3>';
-    var parent = document.getElementById('map');
+    let text = '<h3 id="error-msg">Sorry, we couldn\'t load the map.</h3>';
+    let parent = document.getElementById('map');
     parent.innerHTML = text;
 };
